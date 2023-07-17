3c06e9e30a2ab4781fd8c4fdd139a52c319ceef5
CAMM961001 <miguel.castama@gmail.com>
2023-07-17 16:27:00 -0600

    Extract files from tar

--- 01_extraction.py
+++ 01_extraction.py
@@ -4,7 +4,7 @@ import src.settings as settings
 
 
 DATA_DIR = os.path.join(settings.ROOT, 'data')
-FILE_PATH = os.path.join(DATA_DIR, 'datos-produccion-maiz.tar')
+FILE_PATH = os.path.join(DATA_DIR, 'datos-produccion-maiz.tar.gz')
 
 load.get_tar(name=FILE_PATH, path=DATA_DIR)
 
