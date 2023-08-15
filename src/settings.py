43413cd33bb1cea08055bfad2f00175fdd209a26
CAMM961001 <miguel.castama@gmail.com>
2023-08-15 15:08:22 -0600

    Settings as class

--- src/settings.py
+++ src/settings.py
@@ -1,8 +1,11 @@
 import os
 
-SRC_DIR = os.path.dirname(os.path.abspath(__file__))
-ROOT = os.path.dirname(SRC_DIR)
+class Settings:
+    def __init__(self):
+        self.SRC_DIR = os.path.dirname(os.path.abspath(__file__))
+        self.ROOT = os.path.dirname(self.SRC_DIR)
 
 if __name__ == '__main__':
-    print(ROOT)
+    settings = Settings()
+    print(settings.ROOT)
     
